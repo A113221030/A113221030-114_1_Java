4b504af40682d438e849bd9148d497c900553783
A113221030 <A113221030@mail.shu.edu.tw>
2025-09-19 15:07:35 +0800

    114-09-19

---
+++ 114-09-19/Review_ClassBasic/src/Account.java
@@ -0,0 +1,35 @@
+public class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+            return false;
+        }
+    }
+}
